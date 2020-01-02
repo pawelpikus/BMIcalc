dbca37c70bd036aefe69b15d0f276aff8a125643
Paweł Pikus <pawelpikus7@gmail.com>
2020-01-02 22:51:36 +0100

    2020-01-01 callbacks

--- app.py
+++ app.py
@@ -31,13 +31,48 @@ body = dbc.Container([
 
     ]),
     dbc.Row([
-        html.Div("Calculating patient's Body Mass Index based on their weight and height",
+        html.Div("Calculating patient's Body Mass Index",
                  style={"margin": "auto", "padding-bottom": 60})
+    ]),
+    dbc.Row([
+
+        dbc.Col([
+            dcc.RadioItems(
+                options=[
+                    {'label': 'Male', 'value': 'M'},
+                    {'label': 'Female', 'value': 'F'}
+                ],
+                value='M'
+
+            )
+        ],
+            style={'margin': 'auto', 'padding-bottom': 30},
+            md=4,
+        ),
+
+        dbc.Col([
+            dcc.Dropdown(
+                options=(
+                    {'label': '19 - 24 years old', 'value': '19-24'},
+                    {'label': '25 - 34 years old', 'value': '25-34'},
+                    {'label': '35 - 44 years old', 'value': '35-44'},
+                    {'label': '45 - 54 years old', 'value': '45-54'},
+                    {'label': '55 - 64 years old', 'value': '55-64'},
+                    {'label': 'over 64 years old', 'value': 'over64'}
+                ),
+                placeholder="Select patient's age"
+            )
+        ],
+            style={'margin': 'auto', 'padding-bottom': 30},
+            md=3,
+
+        )
+
     ]),
     dbc.Row([
         dbc.Col(
             html.Div("Set patient's weight:"),
-            width={"size": 6, "offset": 1}
+            width={"size": 6, "offset": 2}
         )
     ]),
     dbc.Row([
@@ -54,7 +89,7 @@ body = dbc.Container([
                 )
 
             ],
-            md=6,
+            md=4,
             style={"margin": "auto"}
         ),
         dbc.Col(
@@ -76,7 +111,7 @@ body = dbc.Container([
     dbc.Row([
         dbc.Col(
             html.Div("Set patient's height:"),
-            width={"size": 6, "offset": 1}
+            width={"size": 6, "offset": 2}
         )
     ]),
     dbc.Row([
@@ -93,7 +128,7 @@ body = dbc.Container([
                 )
 
             ],
-            md=6,
+            md=4,
             style={"margin": "auto", "padding-bottom": 60}
         ),
         dbc.Col(
@@ -122,7 +157,7 @@ body = dbc.Container([
     #   ),
     dbc.Row([
         dbc.Col(
-            dbc.Label('Your BMI is: ', id='bmi-label', style={'font-weight': 'bold', 'font-size': 30}))
+            dbc.Label("Patient's BMI is: ", id='bmi-label', style={'font-weight': 'bold', 'font-size': 30}))
     ],
         className="text-center"),
 
@@ -147,17 +182,17 @@ body = dbc.Container([
                                   6: 'optimal weight',
                                   12: 'overweight',
                                   17: 'obese',
-                                  24: 'fat ass'},
+                                  24: 'ridiculously fat'},
                        },
                 value=0
             )
 
         ])
     ],
-        className="text-center"),
+        className="text-center")
 
 ],
-    fluid=True
+
 )
 
 # dash constructor
@@ -167,25 +202,25 @@ app.layout = html.Div([navbar, body])
 
 
 @app.callback(
-    Output(component_id='bmi_weight', component_property='value'),
-    [Input(component_id='weight-slider', component_property='value')])
+    Output('bmi_weight', 'value'),
+    [Input('weight-slider', 'value')])
 def update_weight(weight_value):
     return weight_value
 
 
 @app.callback(
-    Output(component_id='bmi_height', component_property='value'),
-    [Input(component_id='height-slider', component_property='value')])
+    Output('bmi_height', 'value'),
+    [Input('height-slider', 'value')])
 def update_height(height_value):
     return height_value
 
 
 @app.callback(
     Output('bmi-label', 'children'),
-    [Input('weight-slider', 'value'), Input('height-slider', 'value')])
+    [Input('bmi_weight', 'value'), Input('bmi_height', 'value')])
 def update_label(w_value, h_value):
-    w_value = w_value*10000
-    return "Your BMI is: {:.2f}".format(w_value/(h_value*h_value))
+    w_value = w_value * 10000
+    return "Your BMI is: {:.2f}".format(w_value / (h_value * h_value))
 
 
 @app.callback(
@@ -195,7 +230,7 @@ def update_label(w_value, h_value):
 )
 def update_output(wval, hval):
     wval = wval * 10000
-    return (wval/(hval*hval))-16
+    return (wval / (hval * hval)) - 16
 
 
 # running server
